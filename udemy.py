33d56c3750e8c8f3cebb2e4a35675a6cf175e5fe
Jasbir <shikhawat.jasbir@gmail.com>
2019-08-05 12:03:58 +0530

    Add files via upload

---
+++ udemy.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+
+udemy = open("udemy.txt","w")
+driver = webdriver.Chrome(executable_path='C:\\Users\\JASBIR-PC\\Desktop\\chromedriver_win32\\chromedriver.exe')
+driver.implicitly_wait(10)
+driver.get("https://www.udemy.com/")
+
+#driver.find_elements_by_xpath("//button[@class='carousel-arrow next']").click()
+links = driver.find_elements_by_xpath("//a[@class='merchandising-course-card--mask--2-b-d']")
+for link in links:
+	print(link.get_attribute('href'))
+
+driver.close()
+driver.quit()
